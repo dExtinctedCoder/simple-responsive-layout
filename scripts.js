ea3e8ed1627e6f9c5f9745071eaaf53e7d4fcdc2
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-06-17 00:21:48 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// let input = document.getElementById('email');
+// let input2 = document.getElementById('fName')
+// let input3 = document.getElementById('pWord')
+
+// input.oninvalid = function(event) {
+//   input.oninvalid = function() {
+//     event.target.setCustomValidity("Email should be in the correct format(characters@characters.domain) e.g example@gmail.com")
+//   }
+// }
+// input.oninvalid = function(event) {
+//    event.target.setCustomValidity("Email should be in the correct format(characters@characters.domain) e.g example@gmail.com")
+// }
+// // input2.oninput.oninvalid = function(event2) {
+// //   event2.target.setCustomValidity("First name should be only alphabets")
+// // }
+
+// // input3.oninput.oninvalid = function(event) {
+// //   event.target.setCustomValidity("Password should contain at least one uppercase, one lowercase, a number and a special character($ @ # _ -) e.g John_doe1")
+// // }
